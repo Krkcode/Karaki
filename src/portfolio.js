9d882a493e105cf2a32fb6439d89180fa6a2061b
BoxconnTech <ibradah20@gmail.com>
2021-09-12 00:54:34 +0000

    second comit

---
+++ src/portfolio.js
@@ -0,0 +1,76 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  title: 'KRK.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'KARAKI',
+  role: 'Junior Frontend Dev',
+  description:
+    'Learning Frontend development using 100DaysOfCode, Freedcodecamp and many thanks to my Wonderful Twitter dev Community 🤓 🔥',
+  resume: 'https:twitter.com/Archianti',
+  // social: {
+  //   linkedin: 'https://linkedin.com',
+  //   github: 'https://github.com',
+  // },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'KARAKI',
+    description:
+      'Portfolio website',
+    stack: ['Html', 'Css', 'JavaScript', 'React'],
+    // sourceCode: 'https://github.com',
+    // livePreview: 'https://github.com',
+  },
+  // {
+  //   name: 'Project 2',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 3',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Vercel'
+  // 'TypeScript',
+  // 'React',
+  // 'Redux',
+  // 'SASS',
+  // 'Material UI',
+  // 'Git',
+  // 'CI/CD',
+  // 'Jest',
+  // 'Enzyme',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'ibradah20@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
+
+
+// export { header, about, projects, skills, contact }
